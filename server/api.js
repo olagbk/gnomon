48725c63deb201d7a4dc1fb5ca4bc4cc20b3ab01
Alexandra Gabka <rivvel@gmail.com>
2017-10-09 23:15:13 +0200

    don't require gulp in api.js

--- server/api.js
+++ server/api.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const path = require('path');
-const gulp = require('gulp');
-const tap = require('gulp-tap');
 const router = express.Router();
 
 require('./routes.js')(router);
