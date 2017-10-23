044b2502204ce8ec27d6004e4484493314dee410
Alexandra Gabka <rivvel@gmail.com>
2017-10-24 01:28:07 +0200

    set up tests for Angular

--- app/scripts/app.component.spec.ts
+++ app/scripts/app.component.spec.ts
@@ -1,4 +1,6 @@
+import { should } from 'chai';
 import { TestBed, async } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
@@ -8,25 +10,27 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      schemas: [NO_ERRORS_SCHEMA]
+    });
   }));
 
   it('should create the scripts', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    should().exist(app);
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Gnomon'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    app.title.should.equal('Gnomon');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to scripts!');
+    compiled.querySelector('h1').textContent.should.include('Welcome to Gnomon!');
+
   }));
 });
