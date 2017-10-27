6aa656790b377b3853fbf86a25a2c9db19c1791a
Alexandra Gabka <rivvel@gmail.com>
2017-10-28 00:02:06 +0200

    add and style HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
 
 const ROUTES = [
   {
     path: '',
-    redirectTo: 'posts',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    component: HomeComponent
+   },
   {
     path: 'posts',
     component: PostsComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { PostsComponent } from './posts/posts.component';
 import { NavComponent } from './nav/nav.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
 
 import { PostsService } from './posts.service';
 
@@ -15,7 +16,8 @@ import { PostsService } from './posts.service';
   declarations: [
     AppComponent,
     PostsComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
