234b4cf92f1a0aa9baae99b2d247d16db82ed9e3
Alexandra Gabka <rivvel@gmail.com>
2017-11-01 22:10:52 +0100

    style gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,19 @@ import { FlickrService } from './gallery/flickr.service';
 
 
 export const galleryConfig: GalleryConfig = {
-  // ...
+  'style': {
+    'background': 'rgba(25,25,25,0.8)',
+    'width': '100%',
+    'height': '100%'
+  },
+  'animation': 'fade',
+  'thumbnails': {
+    'position': 'top',
+    'space': 20,
+    'width': 180,
+    'height': 135
+  },
+  'navigation': true
 };
 
 @NgModule({
