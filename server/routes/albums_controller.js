363729256b5612f3f7dfdd7991f23438d41ae167
Alexandra Gabka <rivvel@gmail.com>
2017-11-07 04:13:08 +0100

    fetch albums in a single request

--- server/routes/albums_controller.js
+++ server/routes/albums_controller.js
@@ -1,16 +1,41 @@
 'use strict';
+import Flickr from '../flickr.js';
+import config from '~/config/config.json';
 
 module.exports = (router, sequelize) => {
   router.route('/albums')
-
     .get((req, res) => {
-      sequelize.models.albums.findAll()
-        .then(data => {
-          let response = {};
-          data.forEach(album => response[album.dataValues.type] = album.dataValues.album_id);
-          res.json(response);
+      Flickr
+        .then(flickr => {
+          sequelize.models.albums.findOne({where: {type: req.query.album}})
+            .then(data => {
+              flickr.photosets.getPhotos(
+                {
+                  user_id: config.flickr.nsid,
+                  photoset_id: data.album_id,
+                  page: req.query.page,
+                  per_page: req.query.perPage,
+                  extras: ['url_o', 'url_n']
+                },
+                (err, result) => {
+                  if (err) return res.send(err);
+                  const images = result.photoset.photo.map(img => {
+                    return {
+                      src: `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_b.jpg`,
+                      url: img.url_o,
+                      thumbnail: img.url_n,
+                      text: img.title
+                    }
+                  });
+                  res.json({images: images, count: result.photoset.total})
+                }
+              )
+          })
+            .catch(err => res.send(`Couldn't fetch album ID: ${err.message}`))
         })
-        .catch(err => res.send(err));
+        .catch(err => {
+          res.send(`Can't connect to Flickr API: ${err.message}`)
+        })
     });
 };
 
--- src/app/gallery/flickr.service.ts
+++ src/app/gallery/flickr.service.ts
@@ -5,7 +5,6 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class FlickrService {
-  private galleryUrl = '/api/gallery';
   private albumsUrl = '/api/albums';
   constructor(private http: Http) { }
 
@@ -14,19 +13,14 @@ export class FlickrService {
     return Promise.reject(error.message || error);
   }
   public getImages(options) {
-    const getAlbumId = new Promise((resolve) => {
-      this.http.get(this.albumsUrl)
-        .toPromise()
-        .then(response => {
-          const responseObj = response.json();
-          resolve(responseObj[options.album]);
-        });
-    });
-    return getAlbumId.then(id => {
-      return this.http.get(this.galleryUrl, {params: {album_id: id, page: options.page, perPage: options.perPage}})
-        .toPromise()
-        .then(response => response.json())
-        .catch(err => FlickrService.handleError(err));
-      });
+    if (sessionStorage) {
+      const albumID = sessionStorage.getItem(options.album);
+      if (albumID) { options.albumID = albumID; }
+    }
+    return this.http.get(this.albumsUrl, {params: options})
+      .toPromise()
+      .then(response => response.json())
+      .catch(err => FlickrService.handleError(err));
   }
 }
+
