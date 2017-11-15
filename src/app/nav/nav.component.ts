214f5b6d721ff53ed01d5c0bc5aed4f88205beed
Aleksandra Gabka <rivvel@gmail.com>
2017-11-15 22:12:56 +0100

    clean up nav component and add home button

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavRoute } from './nav-route';
 
 @Component({
   selector: 'app-nav',
@@ -8,19 +7,11 @@ import { NavRoute } from './nav-route';
 })
 export class NavComponent implements OnInit {
   public isCollapsed: boolean;
-  public menu: NavRoute[];
 
   constructor() { }
 
   ngOnInit() {
     this.isCollapsed = false;
-    this.menu = [
-      new NavRoute('Blog', '/blog'),
-      new NavRoute('Drawings', '/drawings'),
-      new NavRoute('Sketches', '/sketches'),
-      new NavRoute('Photographs', '/photos'),
-      new NavRoute('About', '/about'),
-    ];
   }
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
